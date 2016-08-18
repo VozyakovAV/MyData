411eb7b324f417c1cc5da27b96ae28c45ecffe83
andrew <andvoz@mail.ru>
2016-08-18 11:35:23 +0400

    test

---
+++ src/Common/Extensions/List.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class ListExtension
+    {
+        public static T GetRandom<T>(this IEnumerable<T> list)
+        {
+            var r = new Random();
+            int index = r.Next(list.Count());
+            return list.ToList()[index];
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var r = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = r.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
---
+++ src/MyData/Areas/Memory/Controllers/LernController.cs
@@ -0,0 +1,96 @@
+﻿using MyData.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common;
+
+namespace MyData.Areas.Memory.Controllers
+{
+    public class LernController : BaseController
+    {
+        public ActionResult Index(int setID = 0)
+        {
+            var set = mng.Memory.GetSet(setID);
+            if (set == null)
+                return HttpNotFound();
+            var data = new TestData() { SetID = set.Id };
+            Session[DATA_SESSION] = data;
+            
+            return View();
+        }
+
+        private const string DATA_SESSION = "TestData";
+        public JsonResult NextQuestion()
+        {
+            var data = Session[DATA_SESSION] as TestData;
+            if (data == null)
+                return Json(false);
+            var terms = mng.Memory.GetTerms(data.SetID);
+            if (terms.Count == 0)
+                return Json(false);
+
+            var term = terms.GetRandom();
+            var otherAnswers = terms.Where(x => x.Id != term.Id).Select(x => x.Answer).Distinct().Take(3).ToList();
+
+            var q = new TestQuestion();
+            q.Question = term.Question;
+            var answers = new List<string>();
+            answers.Add(term.Answer);
+            answers.AddRange(otherAnswers);
+            answers.Shuffle();
+            q.Answers = answers.ToArray();
+            data.CorrectAnswer = answers.IndexOf(term.Answer);
+
+            return Json(q);
+        }
+
+        public JsonResult CheckAnswer(int answer)
+        {
+            var data = Session[DATA_SESSION] as TestData;
+            if (data == null)
+                return Json(false);
+
+            var res = new CheckAnswerResult();
+            res.CorrectAnswer = data.CorrectAnswer;
+            if (data.CorrectAnswer == answer)
+                res.IsCorrect = true;
+            data.CheckAnswer();
+
+            return Json(res);
+        }
+    }
+
+    public class TestData
+    {
+        public int SetID { get; set; }
+
+        private int _correctAnswer;
+        public int CorrectAnswer
+        {
+            get { return _correctAnswer; }
+            set { _correctAnswer = value; _isChecked = false; }
+        }
+
+        private bool _isChecked;
+        public bool IsChecked { get { return _isChecked; } }
+
+        public void CheckAnswer()
+        {
+            _isChecked = true;
+        }
+    }
+
+    public class TestQuestion
+    {
+        public string Question { get; set; }
+        public string[] Answers { get; set; }
+    }
+
+    public class CheckAnswerResult
+    {
+        public bool IsCorrect { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
